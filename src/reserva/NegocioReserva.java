e836bdc097dc9f9e0feba5aed6b262698e50bbaa
rodri_000 <rodri_000@Zeon>
2016-10-26 21:26:32 -0200

    Adição de Validações para a Classe NegocioReserva.

--- src/reserva/NegocioReserva.java
+++ src/reserva/NegocioReserva.java
@@ -8,9 +8,28 @@ public class NegocioReserva implements InterfacerReserva {
     public void fazerReserva(Reserva r) throws Exception {
         //Fazer as validações
         if (r.getCd_reserva() == 0) {
-            throw new Exception ("Informar um número de reserva válido.");
+            throw new Exception("Informar um Número de Reserva Válido.");
+        }
+
+        if (r.getPeriodo() == 0) {
+            throw new Exception("Informe um Número Válido para o Periodo.");
+        }
+
+        if (r.getCpf_cliente().equals("   .   .   -  ")) {
+            throw new Exception("Informe o CPF Totalmente.");
         }
         
+         if (r.getData().equals("  /  /  ")) {
+            throw new Exception("Informe a Data por Completo.");
+        }
+
+        if (r.getNr_quarto() == 0) {
+            throw new Exception("Informe o Número do Quarto.");
+        }
+
+        if (r.getCd_ocupacao() == 0) {
+            throw new Exception("Informe o Código de Ocupação.");
+        }
     }
 
     @Override
@@ -27,5 +46,5 @@ public class NegocioReserva implements InterfacerReserva {
     public ArrayList<Reserva> pesquisarReserva(Reserva filtro) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-       
+
 }
