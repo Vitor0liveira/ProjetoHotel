31162f07b3170a2e4cbd4f843fc63ecf8c918449
Victor Lauria <victor7lauria@live.com>
2016-10-26 20:11:47 -0200

    Atualização fachada.

--- src/fachada/Fachada.java
+++ src/fachada/Fachada.java
@@ -1,5 +1,61 @@
 package fachada;
 
-public class Fachada {
-    
+import cliente.Cliente;
+import cliente.InterfaceCliente;
+import cliente.NegocioCliente;
+import java.util.ArrayList;
+import reserva.InterfaceReserva;
+import reserva.NegocioReserva;
+import reserva.Reserva;
+
+public class Fachada implements InterfaceCliente, InterfaceReserva {
+
+    @Override
+    public void cadastrarCliente(Cliente c) throws Exception {
+        NegocioCliente nC = new NegocioCliente();
+        nC.cadastrarCliente(c);
+    }
+
+    @Override
+    public void atualizarCliente(Cliente c) throws Exception {
+        NegocioCliente nC = new NegocioCliente();
+        nC.atualizarCliente(c);
+    }
+
+    @Override
+    public void apagarCliente(Cliente c) throws Exception {
+        NegocioCliente nC = new NegocioCliente();
+        nC.apagarCliente(c);
+    }
+
+    @Override
+    public ArrayList<Cliente> pesquisarCliente(Cliente filtro) throws Exception {
+        NegocioCliente nC = new NegocioCliente();
+        return nC.pesquisarCliente(filtro);
+    }
+
+    @Override
+    public void fazerReserva(Reserva r) throws Exception {
+       NegocioReserva nR = new NegocioReserva();
+       nR.fazerReserva(r);
+    }
+
+    @Override
+    public void atualizarReserva(Reserva r) throws Exception {
+      NegocioReserva nR = new NegocioReserva();
+      nR.atualizarReserva(r);
+    }
+
+    @Override
+    public void apagarReserva(Reserva r) throws Exception {
+      NegocioReserva nR = new NegocioReserva();
+      nR.apagarReserva(r);
+    }
+
+    @Override
+    public ArrayList<Reserva> pesquisarReserva(Reserva filtro) throws Exception {
+        NegocioReserva nR = new NegocioReserva();
+        return nR.pesquisarReserva(filtro);
+    }
+
 }
