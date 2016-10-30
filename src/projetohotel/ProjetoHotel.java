638f34ee46a95cd6345eefe58a2ba2f94c3af218
rodri_000 <rodri_000@Zeon>
2016-10-29 23:31:41 -0200

    Remoções de Comentários desnecesários.

--- src/projetohotel/ProjetoHotel.java
+++ src/projetohotel/ProjetoHotel.java
@@ -2,7 +2,7 @@ package projetohotel;
 
 import cliente.Cliente;
 import cliente.DadosCliente;
-
+import conexao.Dados;
 import javax.swing.JOptionPane;
 import reserva.NegocioReserva;
 import reserva.Reserva;
@@ -16,21 +16,23 @@ public class ProjetoHotel {
             d.desconectar();
             JOptionPane.showMessageDialog(null, "Funcionou");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());*/
-
+            JOptionPane.showMessageDialog(null, e.getMessage());
+             */
+ 
             Cliente c = new Cliente();
-            c.setCpf_cliente("109.010.011-20");
-            c.setNm_cliente("MOTHER FUCK");
-            c.setSexo("M");
-            c.setTelefone("9999-9999");
+            c.setCpf_cliente("109.010.011-22");
+            c.setNm_cliente("QWERTYZINHO");
+            c.setTelefone("9999-9998");
+            c.setSexo("F");
 
-            DadosCliente dC = new DadosCliente();
-            dC.cadastrarCliente(c);
+            DadosCliente DC = new DadosCliente();
+            DC.cadastrarCliente(c);
             JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso.");
-
-            /*Para fazer uma reserva precisamos de um Clinte (objeto), pois o mesmo irá conter o cpf
+             
+ /*Para fazer uma reserva precisamos de um Clinte (objeto), pois o mesmo irá conter o cpf
             que será o codigo identificador.
              */
+ 
             Reserva r = new Reserva();
             r.setCd_reserva(4);
             r.setData("19.92.2010");
@@ -43,9 +45,10 @@ public class ProjetoHotel {
             NegocioReserva nr = new NegocioReserva();
             nr.fazerReserva(r);
             JOptionPane.showMessageDialog(null, "Reserva efetuada com sucesso.");
-
+             
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
+
     }
 }
