8049af20275c24d203ab244be8803bf4e3471a36
rodri_000 <rodri_000@Zeon>
2016-10-31 21:54:50 -0200

    Validações em NegocioCliente. 31/10.

--- src/cliente/NegocioCliente.java
+++ src/cliente/NegocioCliente.java
@@ -1,43 +1,23 @@
 package cliente;
 
-import java.util.ArrayList;
-
 public class NegocioCliente implements InterfaceCliente {
 
     @Override
     public void cadastrarCliente(Cliente c) throws Exception {
+
         if (c.getCpf_cliente().trim().length() < 14) {
             throw new Exception("Por favor, preencha o campo CPF.");
+
         }
         if (c.getNm_cliente().trim().equals("")) {
-            throw new Exception ("Por favor, preencha o campo nome.");
+            throw new Exception("Por favor, preencha o campo nome.");
         }
-        if (c.getSexo().trim().equals("")) {
-            throw new Exception("Por favor, preencha o campo sexo. ");
-        }
-        if (c.getTelefone().trim().equals("")) {
+        if (c.getTelefone().equals("(  )      -    ")) {
             throw new Exception("Por favor, preencha o campo telefone.");
         }
-        
+
         DadosCliente dC = new DadosCliente();
         dC.cadastrarCliente(c);
     }
 
-    @Override
-    public void atualizarCliente(Cliente c) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void apagarCliente(Cliente c) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public ArrayList<Cliente> pesquisarCliente(Cliente filtro) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    
 }
